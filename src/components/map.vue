afb9c2f055220cd5fc9a7795b211c3729662af16
Tucker Johnson <100146767+Tuck1297@users.noreply.github.com>
2023-05-11 11:16:14 -0500

    Update map.vue

--- src/components/map.vue
+++ src/components/map.vue
@@ -71,12 +71,12 @@ export default {
       },
       firebase: {
         config: {
-          apiKey: "AIzaSyDDgtEM2eQeQDTYp8U-KtpSEIKcXXy7zng",
-          authDomain: "map-mn.firebaseapp.com",
-          projectId: "map-mn",
-          storageBucket: "map-mn.appspot.com",
-          messagingSenderId: "899284998433",
-          appId: "1:899284998433:web:40e52032a2c3d984968660"
+          apiKey: "key",
+          authDomain: "domain",
+          projectId: "id",
+          storageBucket: "bucket",
+          messagingSenderId: "senderid",
+          appId: "appid"
         },
         app: null,
         db: null,
